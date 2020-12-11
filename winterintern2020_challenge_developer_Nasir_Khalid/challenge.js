7064dc4d04db3333e94e896d38de885059a63e3c
nkha0010 <nasir.khalid.new@gmail.com>
2020-12-11 09:49:23 +0500

    added functionality for age

--- winterintern2020_challenge_developer_Nasir_Khalid/challenge.js
+++ winterintern2020_challenge_developer_Nasir_Khalid/challenge.js
@@ -11,7 +11,18 @@ $("#name").on('blur focus', function(e) {
         case  "focus":
             ask_name();        
         break;
+    }
+});
 
+$("#date").on('blur focus', function(e) {
+    switch(e.type) {
+        case  "blur":
+            get_age();        
+        break;
+
+        case  "focus":
+            prompt_age();        
+        break;
     }
 });
 
@@ -31,3 +42,21 @@ function greet() {
         document.getElementById("nameOutput").innerText = "Hi! Please enter your name ";
         }
     }   
+
+//calculation of age taken from https://stackoverflow.com/questions/4060004/calculate-age-given-the-birth-date-in-the-format-yyyymmdd
+function get_age() {
+    birthdate = document.getElementById("date").value;
+
+    if (birthdate){
+        var birthday = +new Date(birthdate); //converts the object to the integer representation of the date object
+        age =  ~~((Date.now() - birthday) / (31557600000)); //~~ for rounding. Subtract the difference and divide by a year(24 * 3600 * 365.25 * 1000)
+        document.getElementById("ageOutput").innerText = `Hi, you are ${age} years old! Congrats on making it thus far :)`;
+        }
+    else {
+        document.getElementById("ageOutput").innerText = `Would you mind entering your birthdate?`;
+        }
+    }
+
+function prompt_age(){
+    document.getElementById("ageOutput").innerText = `Lemme guess, your age is...`;
+}
